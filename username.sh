ccdf15d66433cab642affbb1548bcd7765f76f61
Alliyah Blacksher <44708393+blacksher@users.noreply.github.com>
2018-11-08 14:04:46 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -4,7 +4,7 @@
 
 echo "Rules: The only characters that can be used are lower case letters, digits, and the underscore character. It must start with a lower case letter, It must contain at least three but no more than 12 characters."
 echo "Enter a username"
-echo name
+read name
 while echo $name | egrep -v "^[a-z][0-9_a-z]{2,11}$" > /dev/null 2>&1
 do
   echo "Enter a username that is greater than 3 characters but less than 12"
